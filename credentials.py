8a80b016114bc263f883481edcc0713c7e9d602f
Charles Edward Baiden <baidenskyress@gmail.com>
2020-08-01 14:28:07 -0400

    base code

---
+++ credentials.py
@@ -0,0 +1,4 @@
+consumer_key = 'TDdNFe5s13NzbKODdSdhXzQB9'
+consumer_secret = 'bLT2ZkPdQWsrltidAO6tRVAvuHz9zDrT07adgzf5Pk5fVEMcSP'
+access_key = '1003611264029949953-3TngjKNkbUMWM7iMMaInWFCKW7qXe5'
+access_secret = '8iPyOCu6kmyBKxhyim8mHBMbY2yJWtd3HdvXiyd3ziv6Q'
